01d283c54764367d52bf8219eea6cf1395147644
sv-makh <sv-makh@mba>
2022-09-16 16:55:24 +0300

    working on duration buttons

--- lib/activity_bloc.dart
+++ lib/activity_bloc.dart
@@ -25,20 +25,41 @@ class ActivityAddedTime extends ActivityEvent {
   ActivityAddedTime({required this.index, required this.interval});
 }
 
-class ActivitiesState {
-  final Box<Activity> activitiesBox;
+class AddedDurationButton extends ActivityEvent {
+  Duration duration;
 
-  ActivitiesState(this.activitiesBox);
+  AddedDurationButton({required this.duration});
+}
+
+class PressedDurationButton extends ActivityEvent {
+  Duration duration;
+
+  PressedDurationButton({required this.duration});
+}
+
+class ActivitiesState{
+  final Box<Activity> activitiesBox;
+  final Map<Duration, bool> durationButtons;
+
+  ActivitiesState(this.activitiesBox, this.durationButtons);
 }
 
 class ActivitiesBloc extends Bloc<ActivityEvent, ActivitiesState> {
   Box<Activity> activitiesBox = Hive.box<Activity>(boxName);
 
-  ActivitiesBloc() : super(ActivitiesState(Hive.box<Activity>(boxName))) {
+  Map<Duration, bool> durationButtons = {
+    Duration(hours: 1): false,
+    Duration(minutes: 30): false,
+  };
+
+  ActivitiesBloc() : super(ActivitiesState(Hive.box<Activity>(boxName), {
+    Duration(hours: 1): false,
+    Duration(minutes: 30): false,
+  })) {
     on<ActivityDeleted>(
         (ActivityDeleted event, Emitter<ActivitiesState> emitter) {
       activitiesBox.deleteAt(event.index);
-      return emitter(ActivitiesState(activitiesBox));
+      return emitter(ActivitiesState(activitiesBox, durationButtons));
     });
 
     on<ActivityAddedTime>(
@@ -46,12 +67,22 @@ class ActivitiesBloc extends Bloc<ActivityEvent, ActivitiesState> {
       Activity activity = activitiesBox.getAt(event.index)!;
       activity.addInterval(event.interval);
       activitiesBox.putAt(event.index, activity);
-      return emitter(ActivitiesState(activitiesBox));
+      return emitter(ActivitiesState(activitiesBox, durationButtons));
     });
 
     on<ActivityAdded>((ActivityAdded event, Emitter<ActivitiesState> emitter) {
       activitiesBox.add(event.activity);
-      return emitter(ActivitiesState(activitiesBox));
+      return emitter(ActivitiesState(activitiesBox, durationButtons));
+    });
+
+    on<AddedDurationButton>((AddedDurationButton event, Emitter<ActivitiesState> emitter) {
+      durationButtons.update(event.duration, (value) => false, ifAbsent: () => false);
+      return emitter(ActivitiesState(activitiesBox, durationButtons));
+    });
+
+    on<PressedDurationButton>((PressedDurationButton event, Emitter<ActivitiesState> emitter) {
+      durationButtons.update(event.duration, (value) => !value, ifAbsent: () => false);
+      return emitter(ActivitiesState(activitiesBox, durationButtons));
     });
   }
 }
--- lib/duration_bottom_sheet.dart
+++ lib/duration_bottom_sheet.dart
@@ -1,102 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:minimal_time_tracker/data/mock_data.dart';
-import 'package:minimal_time_tracker/data/activity.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:minimal_time_tracker/helpers/convert.dart';
+import 'package:minimal_time_tracker/activity_bloc.dart';
 
-class AddActivityScreen extends StatelessWidget {
-  AddActivityScreen({Key? key}) : super(key: key);
-
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _subtitleController = TextEditingController();
+class DurationBottomSheet extends StatelessWidget {
+  DurationBottomSheet({Key? key}) : super(key: key);
 
   final TextEditingController _daysController = TextEditingController();
   final TextEditingController _hoursController = TextEditingController();
   final TextEditingController _minutesController = TextEditingController();
 
-  Set<Duration> _durationButtons = Set<Duration>();
-  List<Duration> _durations = [
-    Duration(hours: 1),
-    Duration(minutes: 30),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.addNewActivity),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: () {
-              Activity _activity = Activity(
-                title: _titleController.text,
-                subtitle: _subtitleController.text,
-              );
-
-              for (var d in _durationButtons) _activity.addDurationButton(d);
-
-              Navigator.pop(
-                context,
-                _activity,
-              );
-            },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Text(AppLocalizations.of(context)!.titleActivity),
-              TextField(
-                controller: _titleController,
-              ),
-              Text(AppLocalizations.of(context)!.subtitleActivity),
-              TextField(
-                controller: _subtitleController,
-              ),
-              //add choise - buttons or table
-              Text(AppLocalizations.of(context)!.addButtons),
-              Row(
-                children: [
-                  for (var d in _durations)
-                    Padding(
-                      padding: EdgeInsets.only(right: 5),
-                      child: OutlinedButton(
-                        child: Text(stringDuration(d, context)),
-                        onPressed: () {
-                          _durationButtons.add(d);
-                        },
-                      ),
-                    ),
-                  OutlinedButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) => _durationPicker(context),
-                      ).then((value) => null);
-                    },
-                    child: Text('+'),
-                  ),
-                ],
-              ),
-              //colorpicker
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _durationPicker(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding:
-            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
         child: SizedBox(
           height: 200,
           child: Center(
@@ -145,11 +65,13 @@ class AddActivityScreen extends StatelessWidget {
                     OutlinedButton(
                       onPressed: () {
                         //нужны начальные знчения и проверка на 0
-                        _durations.add(Duration(
+                        Duration duration = Duration(
                           days: int.parse(_daysController.text),
                           hours: int.parse(_hoursController.text),
                           minutes: int.parse(_minutesController.text),
-                        ));
+                        );
+
+                        BlocProvider.of<ActivitiesBloc>(context).add(AddedDurationButton(duration: duration));
                       },
                       child: Text(AppLocalizations.of(context)!.ok),
                     ),
